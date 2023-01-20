b5dfd6b61b094c9b863d9a25ed928ba7dadacde5
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-01-20 09:16:07 -0500

    Update DiskSpacePieChart.py

--- DiskSpacePieChart.py
+++ DiskSpacePieChart.py
@@ -1,13 +1,28 @@
 from colorama import *
 import shutil
 import matplotlib.pyplot as plt
-total,used,free = shutil.disk_usage("/")
+total,used,free = shutil.disk_usage("D:/")
 
 # data for the pie chart
-labels = ['', 'Free',"used"]
+labels = ['total', 'Free',"used"]
 sizes = [total, free, used]
 
 # create the pie chart
 plt.pie(sizes, labels=labels, autopct='%1.1f%%')
-plt.title("Disk Space Usage")
+plt.title("Disk Space Usage For D drive.")
 plt.show()
+
+
+total,used,free = shutil.disk_usage("C:/")
+
+# data for the pie chart
+labels = ['total', 'Free',"used"]
+sizes = [total, free, used]
+
+# create the pie chart
+plt.pie(sizes, labels=labels, autopct='%1.1f%%')
+plt.title("Disk Space Usage For C drive.")
+plt.show()
+
+
+
