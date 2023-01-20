e67b7028ec1abbf0876f3ddd244219c2c491a991
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-01-20 17:46:17 -0500

    pie

--- DiskSpace.py
+++ DiskSpace.py
@@ -10,6 +10,9 @@ os.system('cls' if os.name=='nt' else "clear")
 def is_drive_connected(drive_letter):
     return os.path.exists(drive_letter + ":")
 
+GREEN = Fore.GREEN
+RED = Fore.RED
+YELLOW = Fore.YELLOW
 
 def drive():
 
@@ -18,24 +21,27 @@ def drive():
         total,used,free = shutil.disk_usage("D:/")
         print("DiskSpace")
         print(f"\n{D}")
-        print(Fore.GREEN + "Total: %d TB " % (total // (1024 * 1024 * 1024 * 1024)))
-        print(Fore.RED + "Used: %d GB " % (used // (2**30)))
-        print(Fore.YELLOW + "Free: %d TB" % (free // (1024 * 1024 * 1024 * 1024))+Fore.RESET)
+        print(GREEN + "Total: %d TB " % (total // (1024 * 1024 * 1024 * 1024)))
+        print(RED + "Used: %d GB " % (used // (2**30)))
+        print(YELLOW + "Free: %d TB" % (free // (1024 * 1024 * 1024 * 1024))+Fore.RESET)
+        
         # this is something that say that the top is the D drive and the bottom is the C drive
-        print("-"*23)
+        print("-"*15)
+
         #! THIS IS THE MAIN DRIVE OF THE COMPUTER
         print(f"{C}")
         total,used,free = shutil.disk_usage("C:/")
-        print(Fore.GREEN +"Total: %d GB " % (total // (2 ** 30)))
-        print(Fore.RED +"Used: %d GB " % (used // (2 ** 30)))
-        print(Fore.YELLOW +"Free: %d GB " % (free // (2 ** 30))+ Fore.RESET)
+        print(GREEN +"Total: %d GB " % (total // (2 ** 30)))
+        print(RED +"Used: %d GB " % (used // (2 ** 30)))
+        print(YELLOW +"Free: %d GB " % (free // (2 ** 30))+ Fore.RESET)
 
     else:
         #! THIS IS THE MAIN DRIVE OF THE COMPUTER
         print(f"{C}")
         total,used,free = shutil.disk_usage("C:/")
-        print(Fore.GREEN + "Total: %d GB " % (total // (2 ** 30)))
-        print(Fore.RED + "Used: %d GB " % (used // (2 ** 30)))
-        print(Fore.YELLOW + "Free: %d GB " % (free // (2 ** 30))+ Fore.RESET)
+        print(GREEN + "Total: %d GB " % (total // (2 ** 30)))
+        print(RED + "Used: %d GB " % (used // (2 ** 30)))
+        print(YELLOW + "Free: %d GB " % (free // (2 ** 30))+ Fore.RESET)
 
 drive() 
+
--- DiskSpacePieChart.py
+++ DiskSpacePieChart.py
@@ -1,28 +1,26 @@
 from colorama import *
 import shutil
 import matplotlib.pyplot as plt
+
+#! D Drive Usage
 total,used,free = shutil.disk_usage("D:/")
 
 # data for the pie chart
-labels = ['total', 'Free',"used"]
-sizes = [total, free, used]
+labels = ['Free',"used"]
+sizes = [free, used]
 
 # create the pie chart
 plt.pie(sizes, labels=labels, autopct='%1.1f%%')
 plt.title("Disk Space Usage For D drive.")
 plt.show()
-
-
+#! C Drive Usage
 total,used,free = shutil.disk_usage("C:/")
 
 # data for the pie chart
-labels = ['total', 'Free',"used"]
-sizes = [total, free, used]
+labels = ['Free',"used"]
+sizes = [free, used]
 
 # create the pie chart
 plt.pie(sizes, labels=labels, autopct='%1.1f%%')
 plt.title("Disk Space Usage For C drive.")
 plt.show()
-
-
-
