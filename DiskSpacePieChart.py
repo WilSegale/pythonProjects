502d63cd44e9de1a508d8f54fb5fedb9d77913ee
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-01-21 06:35:27 -0500

    stuff

--- DiskSpacePieChart.py
+++ DiskSpacePieChart.py
@@ -4,9 +4,11 @@ import matplotlib.pyplot as plt
 total,used,free = shutil.disk_usage("/")
 
 # data for the pie chart
-labels = ['', 'Free',"used"]
-sizes = [total, free, used]
-
+labels = ['Free',"used"]
+sizes = [free, used]
+print(Fore.GREEN + "Total: %d GB " % (total // (2 ** 30)))
+print(Fore.RED + "Used: %d GB " % (used // (2 ** 30)))
+print(Fore.YELLOW + "Free: %d GB " % (free // (2 ** 30))+ Fore.RESET)
 # create the pie chart
 plt.pie(sizes, labels=labels, autopct='%1.1f%%')
 plt.title("Disk Space Usage")
