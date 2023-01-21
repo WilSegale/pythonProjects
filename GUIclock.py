8778b2d6098e121967fed4fcfaa51a493f9e3fee
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-01-21 12:13:43 -0500

    output

---
+++ GUIclock.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+import time
+
+root = tk.Tk()
+root.title("Clock")
+
+def update_time():
+
+    current_time = time.strftime("%I:%M:%S %p")
+    label.config(text = current_time)
+    label.after(1000, update_time)
+
+
+label = tk.Label(root, font = ("calibri", 40, "bold"))
+label.pack()
+
+update_time()
+
+root.mainloop()
