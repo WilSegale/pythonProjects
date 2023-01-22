d285ae373352431c22325c43ad9eac24574449a0
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-01-21 19:30:33 -0500

    diskspace

--- DiskSpacePieChart.py
+++ DiskSpacePieChart.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 total,used,free = shutil.disk_usage("D:/")
 
 # data for the pie chart
-labels = ['', 'Free',"used"]
-sizes = [total, free, used]
+labels = ['Free',"used"]
+sizes = [free, used]
 
 # create the pie chart
 plt.pie(sizes, labels=labels, autopct='%1.1f%%')
