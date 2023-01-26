1b2a69d4a9c4944d3c958694c71b78eee9f524e3
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-01-26 18:04:57 -0500

    pyhton

--- DiskToCSV.py
+++ DiskToCSV.py
@@ -5,11 +5,11 @@ total, used, free = shutil.disk_usage("/")
 
 # Data for the CSV chart
 data = {'Total': [total // (2 ** 30)],
-        'Free': [free // (2 ** 30)],
-        'Used': [used // (2 ** 30)]}
+        'Free':  [free // (2 ** 30)],
+        'Used':  [used // (2 ** 30)]}
 
 # Create a DataFrame from the data
 df = pd.DataFrame(data)
-
 # Output the DataFrame to a CSV file
 df.to_csv('DiskSpace.csv', index=False)
+print(data)
--- clock.py
+++ clock.py
@@ -5,13 +5,15 @@ import os
 
 if platform == "darwin":
     while True:
-        tick = strftime('%H:%M:%S %p')
+        tick = strftime('%I:%M:%S %p')
+
         print(f"Time: {tick}")
         time.sleep(1)
         os.system('clear')
 else:
     while True:
-        tick = strftime('%H:%M:%S %p')
+        tick = strftime('%I:%M:%S %p')
+
         print(f"Time: {tick}")
         time.sleep(1)
         os.system('cls')
