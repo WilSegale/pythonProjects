81c147883eb91703c4e208d32fadd967a63ef1a3
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-02-02 10:19:25 -0500

    Update 3D-barGraph.py

--- 3D-barGraph.py
+++ 3D-barGraph.py
@@ -1,9 +1,27 @@
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import shutil
-
 # Bar graph data
-total,used,free = shutil.disk_usage("/")
+import string
+from ctypes import windll
+
+def get_drives():
+    drives = []
+    bitmask = windll.kernel32.GetLogicalDrives()
+    for letter in string.ascii_uppercase:
+        if bitmask & 1:
+            drives.append(letter)
+        bitmask >>= 1
+
+    return drives
+
+if __name__ == '__main__':
+    print (get_drives())    # On my PC, this prints ['A', 'C', 'D', 'F', 'H']
+    
+print("Input the name of the drive you want to the amount of space remaning on")
+drives = input(">>> ")
+
+total,used,free = shutil.disk_usage(drives+":/")
 total // (2 ** 30)
 free // (2 ** 30)
 used // (2 ** 30)
