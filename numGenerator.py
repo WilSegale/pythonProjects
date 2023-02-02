7acbd60421d0ef4fd2cce37dfba65769b3fc529f
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-02-02 07:43:40 -0500

    python

---
+++ numGenerator.py
@@ -0,0 +1,11 @@
+import random
+
+def generate_code(length):
+    code = ""
+    for i in range(length):
+        if i != 0:
+            code += ", "
+        code += str(random.randint(0, 9))
+    return code
+
+print(generate_code(6))
