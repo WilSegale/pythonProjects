dc409d7bbf6ef0145705fc1dac0eb7c83bd2a4ea
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-03-02 18:03:18 -0500

    Update hard-times-will-make-you-grow.py

--- hard-times-will-make-you-grow.py
+++ hard-times-will-make-you-grow.py
@@ -1,8 +1,13 @@
-import random
 import time
+# Open the text file in read mode
+file = open('DepressionQuotes.txt', 'r');
+# Read the file line by line
+line = file.readline()
+while line:
+    # Print the current line
+    print(line)
+    time.sleep(1)
+    # Read the next line
 
-def hardtimes():
-    file = open("DepressionQuotes.txt")
-    words = file.readlines()
-
-hardtimes();
+# Close the file
+file.close()
