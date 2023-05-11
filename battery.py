d7d724b470ee1ba9f31513e8e2dff103b41d88ae
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-05-11 17:42:33 -0400

    Update battery.py

--- battery.py
+++ battery.py
@@ -1,4 +1,4 @@
-    # python script showing battery details
+# python script showing battery details
 from colorama import *
 import psutil
 
@@ -9,28 +9,27 @@ BRIGHT = Style.BRIGHT
 RESET = Style.RESET_ALL
 
 battery = psutil.sensors_battery()
+
 def convertTime(seconds):
     minutes, seconds = divmod(seconds, 60)
     hours, minutes = divmod(minutes, 60)
     return "%d:%02d:%02d" % (hours, minutes, seconds)
-'''
+
 if battery.power_plugged == False:
     batteryPulgedIn = f"{BRIGHT}{YELLOW}Power plugged in: {RED}{battery.power_plugged}{RESET}"
 
 else:
     batteryPulgedIn = f"{BRIGHT}{YELLOW}Power plugged in: {GREEN}{battery.power_plugged}{RESET}"
-'''
+
 if battery:
     battery = psutil.sensors_battery()
-   
     if battery.percent >= int(50):
         print(f"{BRIGHT}{YELLOW}Battery percentage: {GREEN}{battery.percent}%{RESET}")
-        print(f"{BRIGHT}{YELLOW}Power plugged in: {RED}{battery.power_plugged}{RESET}")
+        print(batteryPulgedIn)
         print(f"{BRIGHT}{YELLOW}Battery left: {GREEN}{convertTime(battery.secsleft)}{RESET}")
     else:
-        print(f"{BRIGHT}{YELLOW}Battery percentage: {YELLOW}***{RED}{battery.percent}%{YELLOW}***{RESET}")
+        print(f"{BRIGHT}{YELLOW}Battery percentage: {RED}{battery.percent}%{RESET}")
         print(f"{YELLOW}Power plugged in: {GREEN}{battery.power_plugged}{RESET}")
         print(f"{YELLOW}Battery left: {RED}{convertTime(battery.secsleft)}{RESET}")
-
 else:
     print(f"{RED}{BRIGHT}No battery found.")
