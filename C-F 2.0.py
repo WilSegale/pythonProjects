7b35f03de661966574b1427e82822558006f2b54
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-05-11 16:21:32 -0400

    python

--- C-F 2.0.py
+++ C-F 2.0.py
@@ -1,9 +1,9 @@
+exit = ["exit","quit","EXIT","QUIT"]
+
 def c_to_f(c):
     f = (9/5) * c + 32
     return f
+
 while True:
     c = float(input("Enter the temperature in Celsius: "))
     print("Temperature in Fahrenheit: ", c_to_f(c))
-    
-    if c == "exit":
-        break
