9d658a9a694d4e7a253b38570dcbaef831afba34
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-05-11 17:27:23 -0400

    Update battery.py

--- battery.py
+++ battery.py
@@ -17,11 +17,11 @@ if battery:
     battery = psutil.sensors_battery()
    
     if battery.percent >= int(50):
-        print(f"{YELLOW}Battery percentage: {GREEN}{battery.percent}{RESET}")
-        print(f"{YELLOW}Power plugged in: {GREEN}{battery.power_plugged}{RESET}")
-        print(f"{YELLOW}Battery left: {GREEN}{convertTime(battery.secsleft)}{RESET}")
+        print(f"{BRIGHT}{YELLOW}Battery percentage: {GREEN}{battery.percent}{RESET}")
+        print(f"{BRIGHT}{YELLOW}Power plugged in: {GREEN}{battery.power_plugged}{RESET}")
+        print(f"{BRIGHT}{YELLOW}Battery left: {GREEN}{convertTime(battery.secsleft)}{RESET}")
     else:
-        print(f"{YELLOW}Battery percentage: {RED}{battery.percent}{RESET}")
+        print(f"{BRIGHT}{YELLOW}Battery percentage: {RED}{battery.percent}{RESET}")
         print(f"{YELLOW}Power plugged in: {RED}{battery.power_plugged}{RESET}")
         print(f"{YELLOW}Battery left: {RED}{convertTime(battery.secsleft)}{RESET}")
 else:
