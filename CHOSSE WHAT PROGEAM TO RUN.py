f6041068bab81bfb135f981d6efda7b8a0b576de
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-05-19 11:50:28 -0400

    Update CHOSSE WHAT PROGEAM TO RUN.py

--- CHOSSE WHAT PROGEAM TO RUN.py
+++ CHOSSE WHAT PROGEAM TO RUN.py
@@ -1,6 +1,13 @@
 import os
+from sys import platform
+
 def start():
-   os.system("dir");
-   CHOICE = input("Enter your choice: ");
-   os.system(f"python3 {CHOICE}");
+   if platform == "win32":
+      os.system("dir");
+      CHOICE = input("Enter your choice: ");
+      os.system(f"python3 {CHOICE}");
+   else:
+      os.system("ls -l {CHOICE}");
+      CHOICE = input("Enter your choice: ");
+      os.system(f"python3 {CHOICE}");
 start()
