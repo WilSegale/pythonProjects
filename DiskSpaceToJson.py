ab724c507e6f4aed6aa80cd07401336857b6f03b
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-05-19 10:17:45 -0400

    Update DiskSpaceToJson.py

--- DiskSpaceToJson.py
+++ DiskSpaceToJson.py
@@ -2,11 +2,13 @@ import json
 import shutil
 total, used, free = shutil.disk_usage("/")
 
-data = {"Total-GB": total // (2 ** 30), 
-        "Used-GB": used // (2 ** 30),
-        "Free-GB":free // (2 ** 30)
-        }
-json_data = json.dumps(data)
+data = {
+    "DiskSpace for MAIN DRIVE":[
+        {"Total-GB": total // (2 ** 30)}, 
+        {"Used-GB": used // (2 ** 30)},
+        {"Free-GB":free // (2 ** 30)}
+    ]}
+json_data = json.dumps(data,indent=4)
 
 with open("DiskSpace.json", "w") as outfile:
     outfile.write(json_data)
