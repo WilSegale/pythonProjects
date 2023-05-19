331c0ce3916466dd36cd369c39cb460858e28d01
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-05-19 10:38:44 -0400

    Update DiskSpaceToJson.py

--- DiskSpaceToJson.py
+++ DiskSpaceToJson.py
@@ -1,22 +1,14 @@
 import json
 import shutil
-from colorama import *
-import matplotlib.pyplot as plt
-import plotly.graph_objects as go
-import shutil
+import string
 from colorama import *
 from sys import platform
-import string
 
 GREEN = Fore.GREEN
 RESET = Fore.RESET
-total, used, free = shutil.disk_usage("/")
-import platform
-import string
-from ctypes import windll
-
-def get_drives():
-    if platform.system() == "Windows":
+if platform == "win32":
+    from ctypes import windll
+    def get_drives():
         drives = []
         bitmask = windll.kernel32.GetLogicalDrives()
         for letter in string.ascii_uppercase:
@@ -26,19 +18,20 @@ def get_drives():
 
         return drives
 
-print(f"Drives: {get_drives()}")  # This will print the list of available drives on Windows
-drives = input(">>> ")
+    if __name__ == '__main__':
+        print(f"drive: {get_drives()}")   # On my PC, this prints ['A', 'C', 'D', 'F', 'H']
 
+drive = input(f"Drive: ")
+total, used, free = shutil.disk_usage(f"{drive}:/")
 
 data = {
-    f"DiskSpace for {drives}":[
-        {"Total-GB": total // (2 ** 30)}, 
+    f"DiskSpace for drive:{drive}":[
+        {"Total-GB": total // (2 ** 30)},
         {"Used-GB": used // (2 ** 30)},
-        {"Free-GB":(free // (2 ** 30))}
+        {"Free-GB":free // (2 ** 30)}
     ]}
-json_data = json.dumps(data,indent=2)
+json_data = json.dumps(data,indent=1)
 
 with open("DiskSpace.json", "w") as outfile:
     outfile.write(json_data)
 print(f'{GREEN}Done. It is located in the file called: DiskSpace.json{RESET}')
-get_drives()
