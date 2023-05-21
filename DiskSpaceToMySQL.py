87a9f028933cce908a9b0f39432065bdeb8c4e7e
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-05-21 14:36:47 -0400

    script

--- DiskSpaceToMySQL.py
+++ DiskSpaceToMySQL.py
@@ -14,7 +14,7 @@ mydb = mysql.connector.connect(
   host="localhost",
   user="root",
   password="Cmonster2006!@#!@#!@#",
-  database="NAME_OFDISKSPACE"
+  database="NAME_OF_DISKSPACE"
 )
 
 mycursor = mydb.cursor()
