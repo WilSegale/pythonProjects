41ac883b18edaefe04bdc0751d6c27128f7ab2c5
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-05-27 08:27:40 -0400

    send and recive

---
+++ sender.py
@@ -0,0 +1,19 @@
+import socket
+
+# Create a socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Set up the connection details
+host = '192.168.1.38'  # IP address of the receiving computer
+port = 12345  # Choose a suitable port number
+
+# Connect to the receiver
+s.connect((host, port))
+
+# Send data
+data = b'Hello, receiver!'
+s.send(data)
+
+# Close the socket
+s.close()
+
