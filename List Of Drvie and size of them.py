9eab23a46693744b4dfdec739052c3e4fd11a42a
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-06-04 11:26:55 -0400

    Update List Of Drvie and size of them.py

--- List Of Drvie and size of them.py
+++ List Of Drvie and size of them.py
@@ -1,35 +1,49 @@
 from colorama import *
 import os
 import matplotlib.pyplot as plt
+import readline
 
 GREEN = Fore.GREEN
 RESET = Fore.RESET
 
 def get_drive_capacity(drive_path):
-   total_bytes = os.statvfs(drive_path).f_blocks * os.statvfs(drive_path).f_frsize
-   free_bytes = os.statvfs(drive_path).f_bavail * os.statvfs(drive_path).f_frsize
-   used_bytes = total_bytes - free_bytes
+    total_bytes = os.statvfs(drive_path).f_blocks * os.statvfs(drive_path).f_frsize
+    free_bytes = os.statvfs(drive_path).f_bavail * os.statvfs(drive_path).f_frsize
+    used_bytes = total_bytes - free_bytes
     
-   return used_bytes
+    return used_bytes
 
 directory = "/Volumes"
 
 os.system(f"ls -l {directory}")
 
 print(f"{GREEN}Enter the external hard drive ex(EXTERNAL_HARDWARE){RESET}")
+
+# Define autocomplete function for external hard drive input
+def autocomplete(text, state):
+    options = [name for name in os.listdir(directory) if name.startswith(text)]
+    if state < len(options):
+        return options[state]
+    else:
+        return None
+
+# Enable autocomplete for input_path
+readline.parse_and_bind('tab: complete')
+readline.set_completer(autocomplete)
+
 input_path = input(">>> ")
 
 # List of drive paths
-drive_paths = ["/", f"/Volumes/{input_path}"]  # input the list of the drive path
+drive_paths = ["/", f"/Volumes/{input_path}"]
 
 drive_names = []
 used_capacities = []
 
 # Iterate over drive paths
 for drive_path in drive_paths:
-   drive_capacity = get_drive_capacity(drive_path)
-   drive_names.append(drive_path)
-   used_capacities.append(drive_capacity)
+    drive_capacity = get_drive_capacity(drive_path)
+    drive_names.append(drive_path)
+    used_capacities.append(drive_capacity)
 
 # Plotting the bar graph
 fig, ax = plt.subplots()
