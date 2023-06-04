392071cd718e296c30ca422c1a7f451b89f869a3
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-06-04 17:19:21 -0400

    Update List Of Drvie and size of them.py

--- List Of Drvie and size of them.py
+++ List Of Drvie and size of them.py
@@ -3,6 +3,8 @@ import os
 import matplotlib.pyplot as plt
 import readline
 
+from numpy import arange
+
 GREEN = Fore.GREEN
 RESET = Fore.RESET
 
@@ -10,9 +12,11 @@ def get_drive_capacity(drive_path):
     total = os.statvfs(drive_path).f_blocks * os.statvfs(drive_path).f_frsize
     free = os.statvfs(drive_path).f_bavail * os.statvfs(drive_path).f_frsize
     used = total - free
+    print(f"Total: {total // (2 ** 30)}")
+    print(f"Free: {free // (2 ** 30)}")
+    print(f"Used: {used // (2 ** 30)}")
+    return used, free, total
 
-    print(used // (2 ** 30))
-    return used
 
 directory = "/Volumes"
 
@@ -33,52 +37,83 @@ readline.parse_and_bind('tab: complete')
 readline.set_completer(autocomplete)
 
 input_path = input(">>> ")
+# if the user inputs only "/" it only shows the main drive
 if input_path == "/":
     drive_paths = ["/"]
+
     
     drive_names = []
+
+    total_capacities = []
+    free_capacities = []
     used_capacities = []
 
     # Iterate over drive paths
     for drive_path in drive_paths:
-        drive_capacity = get_drive_capacity(drive_path)
+        used_capacity, free_capacity, total_capacity = get_drive_capacity(drive_path)
         drive_names.append(drive_path)
-        used_capacities.append(drive_capacity)
+        used_capacities.append(used_capacity // (2 ** 30))
+        free_capacities.append(free_capacity  // (2 ** 30))
+        total_capacities.append(total_capacity  // (2 ** 30)) 
 
-    # Plotting the bar graph
+
     fig, ax = plt.subplots()
-    ax.bar(drive_names, used_capacities)
+    bar_width = 100
+
+    # Calculate the positions of the bars
+    index = arange(len(drive_names))
+
+    # Plot the bars
+    ax.bar(index, used_capacities, bar_width, label='Used')
+    ax.bar(index + bar_width, free_capacities, bar_width, label='Free')
+    ax.bar(index + 2 * bar_width, total_capacities, bar_width, label='Total')
 
     ax.set_xlabel('Drive')
-    ax.set_ylabel('Used Capacity')
-    ax.set_title('Used Capacity of Drives')
-
-    plt.xticks(rotation=45)
+    ax.set_ylabel('Capacity')
+    ax.set_title(f'Capacity of Drives for {drive_paths}')
+    ax.set_xticks(index + bar_width)
+    ax.set_xticklabels(drive_names, rotation=45)
+    ax.legend()
 
     plt.show()
-
+elif input_path == "":
+    print('ERROR: INPUT PATH MUST BE SPECIFIED.')
+    
 # List of drive paths
 else:
-    drive_paths = ["/", f"/Volumes/{input_path}"]
-    
+    drive_paths = [f"/Volumes/{input_path}"]
+
     drive_names = []
+
+    total_capacities = []
+    free_capacities = []
     used_capacities = []
 
     # Iterate over drive paths
     for drive_path in drive_paths:
-        drive_capacity = get_drive_capacity(drive_path)
+        used_capacity, free_capacity, total_capacity = get_drive_capacity(drive_path)
         drive_names.append(drive_path)
-        used_capacities.append(drive_capacity)
+        used_capacities.append(used_capacity // (2 ** 30))
+        free_capacities.append(free_capacity  // (2 ** 30))
+        total_capacities.append(total_capacity  // (2 ** 30)) 
 
-    # Plotting the bar graph
+
     fig, ax = plt.subplots()
-    ax.bar(drive_names, used_capacities)
+    bar_width = 100
+
+    # Calculate the positions of the bars
+    index = arange(len(drive_names))
+
+    # Plot the bars
+    ax.bar(index, used_capacities, bar_width, label='Used')
+    ax.bar(index + bar_width, free_capacities, bar_width, label='Free')
+    ax.bar(index + 2 * bar_width, total_capacities, bar_width, label='Total')
 
     ax.set_xlabel('Drive')
-    ax.set_ylabel('Used Capacity')
-    ax.set_title('Used Capacity of Drives')
-
-    plt.xticks(rotation=45)
+    ax.set_ylabel('Capacity')
+    ax.set_title(f'Capacity of Drives for {drive_paths}')
+    ax.set_xticks(index + bar_width)
+    ax.set_xticklabels(drive_names, rotation=45)
+    ax.legend()
 
     plt.show()
-
