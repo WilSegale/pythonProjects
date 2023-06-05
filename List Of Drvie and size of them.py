a2bec95fac581f1cd6ff7381a27a189eca9174ef
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-06-05 10:54:13 -0400

    python

--- List Of Drvie and size of them.py
+++ List Of Drvie and size of them.py
@@ -4,6 +4,15 @@ from colorama import *
 import readline
 import os
 
+quit = ["exit", 
+        "quit", 
+        "EXIT()",
+        "EXIT",
+        "Exit",
+        "QUIT",
+        "Quit",
+        "QUIT()"]
+
 GREEN = Fore.GREEN
 RESET = Fore.RESET
 
@@ -33,86 +42,88 @@ def autocomplete(text, state):
 # Enable autocomplete for input_path
 readline.parse_and_bind('tab: complete')
 readline.set_completer(autocomplete)
+while True:
+    input_path = input(">>> ")
+    if input_path in quit:
+        print("[-] Exiting...")
+        break
+    # if the user inputs only "/" it only shows the main drive
+    if input_path == "/":
+        drive_paths = ["/"]
 
-input_path = input(">>> ")
+        drive_names = []
 
-# if the user inputs only "/" it only shows the main drive
-if input_path == "/":
-    drive_paths = ["/"]
+        total_capacities = []
+        free_capacities = []
+        used_capacities = []
 
-    drive_names = []
-
-    total_capacities = []
-    free_capacities = []
-    used_capacities = []
-
-    # Iterate over drive paths
-    for drive_path in drive_paths:
-        used_capacity, free_capacity, total_capacity = get_drive_capacity(drive_path)
-        drive_names.append(drive_path)
-        used_capacities.append(used_capacity // (2 ** 30))
-        free_capacities.append(free_capacity  // (2 ** 30))
-        total_capacities.append(total_capacity  // (2 ** 30)) 
-
-
-    fig, ax = plt.subplots()
-    bar_width = 100
-
-    # Calculate the positions of the bars
-    index = arange(len(drive_names))
-
-    # Plot the bars
-    ax.bar(index, used_capacities, bar_width, label='Used')
-    ax.bar(index + bar_width, free_capacities, bar_width, label='Free')
-    ax.bar(index + 2 * bar_width, total_capacities, bar_width, label='Total')
-
-    ax.set_xlabel('Drive')
-    ax.set_ylabel('Capacity')
-    ax.set_title(f'Capacity of Drives for {drive_paths}')
-    ax.set_xticks(index + bar_width)
-    ax.set_xticklabels(drive_names, rotation=45)
-    ax.legend()
-
-    plt.show()
-    
-elif input_path == "":
-    print('ERROR: INPUT PATH MUST BE SPECIFIED.')
-
-# List of drive paths
-else:
-    drive_paths = [f"/Volumes/{input_path}"]
-
-    drive_names = []
-
-    total_capacities = []
-    free_capacities = []
-    used_capacities = []
-
-    # Iterate over drive paths
-    for drive_path in drive_paths:
-        used_capacity, free_capacity, total_capacity = get_drive_capacity(drive_path)
-        drive_names.append(drive_path)
-        used_capacities.append(used_capacity // (2 ** 30))
-        free_capacities.append(free_capacity  // (2 ** 30))
-        total_capacities.append(total_capacity  // (2 ** 30)) 
+        # Iterate over drive paths
+        for drive_path in drive_paths:
+            used_capacity, free_capacity, total_capacity = get_drive_capacity(drive_path)
+            drive_names.append(drive_path)
+            used_capacities.append(used_capacity // (2 ** 30))
+            free_capacities.append(free_capacity  // (2 ** 30))
+            total_capacities.append(total_capacity  // (2 ** 30)) 
 
 
-    fig, ax = plt.subplots()
-    bar_width = 100
+        fig, ax = plt.subplots()
+        bar_width = 100
 
-    # Calculate the positions of the bars
-    index = arange(len(drive_names))
+        # Calculate the positions of the bars
+        index = arange(len(drive_names))
 
-    # Plot the bars
-    ax.bar(index, used_capacities, bar_width, label='Used')
-    ax.bar(index + bar_width, free_capacities, bar_width, label='Free')
-    ax.bar(index + 2 * bar_width, total_capacities, bar_width, label='Total')
+        # Plot the bars
+        ax.bar(index, used_capacities, bar_width, label='Used')
+        ax.bar(index + bar_width, free_capacities, bar_width, label='Free')
+        ax.bar(index + 2 * bar_width, total_capacities, bar_width, label='Total')
 
-    ax.set_xlabel('Drive')
-    ax.set_ylabel('Capacity')
-    ax.set_title(f'Capacity of Drives for {drive_paths}')
-    ax.set_xticks(index + bar_width)
-    ax.set_xticklabels(drive_names, rotation=45)
-    ax.legend()
+        ax.set_xlabel('Drive')
+        ax.set_ylabel('Capacity')
+        ax.set_title(f'Capacity of Drives for {drive_paths}')
+        ax.set_xticks(index + bar_width)
+        ax.set_xticklabels(drive_names, rotation=45)
+        ax.legend()
 
-    plt.show()
+        plt.show()
+        
+    elif input_path == "":
+        print('ERROR: INPUT PATH MUST BE SPECIFIED.')
+
+    # List of drive paths
+    else:
+        drive_paths = [f"/Volumes/{input_path}"]
+
+        drive_names = []
+
+        total_capacities = []
+        free_capacities = []
+        used_capacities = []
+
+        # Iterate over drive paths
+        for drive_path in drive_paths:
+            used_capacity, free_capacity, total_capacity = get_drive_capacity(drive_path)
+            drive_names.append(drive_path)
+            used_capacities.append(used_capacity // (2 ** 30))
+            free_capacities.append(free_capacity  // (2 ** 30))
+            total_capacities.append(total_capacity  // (2 ** 30)) 
+
+
+        fig, ax = plt.subplots()
+        bar_width = 100
+
+        # Calculate the positions of the bars
+        index = arange(len(drive_names))
+
+        # Plot the bars
+        ax.bar(index, used_capacities, bar_width, label='Used')
+        ax.bar(index + bar_width, free_capacities, bar_width, label='Free')
+        ax.bar(index + 2 * bar_width, total_capacities, bar_width, label='Total')
+
+        ax.set_xlabel('Drive')
+        ax.set_ylabel('Capacity')
+        ax.set_title(f'Capacity of Drives for {drive_paths}')
+        ax.set_xticks(index + bar_width)
+        ax.set_xticklabels(drive_names, rotation=45)
+        ax.legend()
+
+        plt.show()
