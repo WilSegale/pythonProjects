02698cb48c97698ef3dd7dced63f5491804eaf0c
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-06-06 20:17:55 -0400

    Update PieChartOfDiskSpace.py

--- PieChartOfDiskSpace.py
+++ PieChartOfDiskSpace.py
@@ -1,26 +1,41 @@
-import shutil
 import matplotlib.pyplot as plt
+from colorama import *
+import shutil
+
+lightblue = Fore.BLUE
+Gray = Fore.LIGHTBLACK_EX
+RESET = Fore.RESET
+
+
 total,used,free = shutil.disk_usage("/")
 
 # Disk space data
 disk_space = {
     total // (2 ** 30):total,
-    used // (2 ** 30):used,
-    free // (2 ** 30):free
+    used // (2 ** 30):used
 }
 
 # Pie chart
 labels = list(disk_space.keys())
 sizes = list(disk_space.values())
+explode = (0.1, 10, 0)  # Explode the first slice
+
+# Custom colors
+colors = ['skyblue', 'lightgray']
 
 # Create a pie chart
-plt.pie(sizes, labels=labels, autopct='%1.1f%%')
+plt.pie(sizes, labels=labels, colors=colors, autopct='%1.01f%%', startangle=59, shadow=True)
 
 # Aspect ratio
-plt.axis('equal')
+plt.axis('on')
 
 # Title
 plt.title('Disk Space')
+
+# Legend
+plt.legend(labels, loc='best')
+print(f"FREE: {lightblue}{total // (2 ** 30)}{RESET}")
+print(f"USED: {Gray}{used // (2 ** 30)}{RESET}")
 
 # Display the chart
 plt.show()
