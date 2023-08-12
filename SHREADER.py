0048994c5d01567229d539b10abda7bae166ebcd
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-08-12 14:44:13 -0400

    Update SHREADER.py

--- SHREADER.py
+++ SHREADER.py
@@ -1,12 +1,13 @@
 import random
 import string
 FILE = input("Enter the file name: ")
+length = input("Enter the desired length of the string: ")
 def generate_random_string(length):
     characters = string.ascii_letters + string.digits  # combining letters and digits
     random_string = ''.join(random.choice(characters) for _ in range(length))
     return random_string
 
-random_string = generate_random_string(100)  # Replace 10 with the desired length of the string
+random_string = generate_random_string(int(length))  # Replace 10 with the desired length of the string
 
 with open(FILE, 'w') as file:
     file.write(random_string)
