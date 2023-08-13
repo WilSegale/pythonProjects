04c5f26460cbb71e63615c9ac9164b84542c0c47
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-08-13 12:02:16 -0400

    python

---
+++ IsMyDiskEncryptedForMac.py
@@ -0,0 +1,24 @@
+import subprocess
+
+def is_disk_encrypted(disk_path):
+    try:
+        output = subprocess.check_output(["diskutil", "info", "-plist", disk_path])
+        output = output.decode("utf-8")
+        
+        # Look for the string indicating encryption
+        if "FileVault" in output:
+            return True
+        else:
+            return False
+    except subprocess.CalledProcessError:
+        return False
+
+# Example usage
+disk_path = input("Enter the disk identifier (e.g., /dev/disk1): ")
+encrypted = is_disk_encrypted(disk_path)
+
+if encrypted:
+    print(f"The disk '{disk_path}' is encrypted.")
+else:
+    print(f"The disk '{disk_path}' is not encrypted.")
+
