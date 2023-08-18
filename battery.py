cdbc4d0e0c6357d28e8b593a8453cf9c306e33e3
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-08-18 17:48:41 -0400

    Update battery.py

--- battery.py
+++ battery.py
@@ -14,13 +14,13 @@ def convertTime(seconds):
 
 if battery:
     battery = psutil.sensors_battery()
-    if battery.percent >= int(50):
+    if battery.percent <= int(50):
         print(f"{BRIGHT}{YELLOW}Battery percentage: {GREEN}{battery.percent}%{RESET}")
         print(f"{BRIGHT}{YELLOW}Power plugged in: {RED}{battery.power_plugged}{RESET}")
         print(f"{BRIGHT}{YELLOW}Battery left: {GREEN}{convertTime(battery.secsleft)}{RESET}")
     else:
-        print(f"{BRIGHT}{YELLOW}Battery percentage: {RED}{battery.percent}%{RESET}")
-        print(f"{YELLOW}Power plugged in: {GREEN}{battery.power_plugged}{RESET}")
+        print(f"{BRIGHT}{YELLOW}Battery percentage: {GREEN}{battery.percent}%")
+        print(f"{YELLOW}Power plugged in: {GREEN}{battery.power_plugged}")
         print(f"{YELLOW}Battery left: {RED}{convertTime(battery.secsleft)}{RESET}")
 else:
     print(f"{RED}{BRIGHT}[-] No battery found.{RESET}")
