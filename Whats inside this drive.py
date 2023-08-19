bfe63706a9c97b62bef3c12d056b27d9af4fe5a0
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-08-19 09:47:04 -0400

    what is my drive

--- Whats inside this drive.py
+++ Whats inside this drive.py
@@ -1,9 +1,30 @@
 import os
 
-os.system("ls -l /Volumes")
-Drive = input("Drive: ")
-external_drive_path = f"/Volumes/{Drive}"  # Replace with the correct name of your external drive
-os.chdir(external_drive_path)
+def list_drive_contents(drive_path):
+    try:
+        contents = os.listdir(drive_path)
+        return contents
+    except OSError as e:
+        print(f"Error: {e}")
+        return []
 
-contents = os.listdir()
-print(contents)
+def main():
+    drives = os.listdir("/Volumes")
+    print("Available drives:")
+    for drive in drives:
+        print(drive)
+    
+    drive_name = input("Enter the name of the drive: ")
+    external_drive_path = os.path.join("/Volumes", drive_name)
+    
+    if not os.path.exists(external_drive_path):
+        print("Drive not found.")
+        return
+    
+    contents = list_drive_contents(external_drive_path)
+    print("Contents of the drive:")
+    for item in contents:
+        print(item)
+
+if __name__ == "__main__":
+    main()
