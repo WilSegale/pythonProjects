b1d93eb9fe678c267ba33cbf0607a15e97b82dc6
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-09-01 09:55:45 -0400

    output

--- TextToJson.py
+++ TextToJson.py
@@ -2,7 +2,6 @@
 # file to JSON
 import json
 
-
 # the file to be converted
 filename = 'data.txt'
 
@@ -10,41 +9,38 @@ filename = 'data.txt'
 dict1 = {}
 
 # fields in the sample file
-fields =['name', 'designation']
+fields = ['name', 'designation']
 
 with open(filename) as fh:
-	
-	# count variable for employee id creation
-	countLine = 1
-	
-	for line in fh:
-		
-		# reading line by line from the text file
-		description = list( line.strip().split(None, 4))
-		
-		# for output see below
-		print(description)
-		
-		# for automatic creation of id for each employee
-		sno ='emp'+str(Loopvar)
-	
-		# loop variable
-		Loopvar = 0
-		# intermediate dictionary
-		dict2 = {}
-		while Loopvar<len(fields):
-			
-				# creating dictionary for each employee
-				dict2[fields[Loopvar]]= description[Loopvar]
-				Loopvar = Loopvar + 1
-				
-		# appending the record of each employee to
-		# the main dictionary
-		dict1[sno]= dict2
-		Loopvar = Loopvar + 1
+    
+    # count variable for employee id creation
+    countLine = 1
+    
+    for line in fh:
+        
+        # reading line by line from the text file
+        description = line.strip().split(None, len(fields))  # Use len(fields) as split limit
+        
+        # for automatic creation of id for each employee
+        Loopvar = 0  # Moved Loopvar initialization here
+        sno = 'emp' + str(Loopvar)
+    
+        # intermediate dictionary
+        dict2 = {}
+        for Loopvar in range(len(fields)):
+            
+            # creating dictionary for each employee
+            if Loopvar < len(description):
+                dict2[fields[Loopvar]] = description[Loopvar]
+            else:
+                dict2[fields[Loopvar]] = ""  # Fill missing fields with empty strings
+        
+        # appending the record of each employee to
+        # the main dictionary
+        dict1[sno] = dict2
+        # Increment Loopvar here if needed
 
-
-# creating json file	
+# creating json file    
 out_file = open("output.json", "w")
-json.dump(dict1, out_file, indent = 4)
+json.dump(dict1, out_file, indent=4)
 out_file.close()
