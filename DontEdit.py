4b426762f1e5382a2222275a815f237b3fe069d6
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-09-02 10:34:36 -0400

    dont edit

--- DontEdit.py
+++ DontEdit.py
@@ -1,18 +1,22 @@
 from colorama import *
 import os
 import time
+# color for the bar graph
 colors = ['green', 
           'red', 
           'yellow']
 
+
+# the quit array that will be used to quit any program that uses it
 quit = ["QUIT",
         "Quit",
         "QUIT()"]
 
-RED = Fore.RED;
+#colors for the program to see
+RED = Fore.RED
 GREEN = Fore.GREEN
 BLUE = Fore.BLUE
-YELLOW = Fore.YELLOW;
+YELLOW = Fore.YELLOW
 ORANGE_Start = "\033[38;2;255;165;0m"
 ORANGE_END = "\033[0m"
 BRIGHT = Style.BRIGHT
