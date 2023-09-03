c21623ed978c9ee764857ead0b46958569266360
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-09-03 12:35:05 -0400

    random

--- RandomColor.py
+++ RandomColor.py
@@ -1,8 +1,9 @@
-from colorama import *
+from DontEdit import *
 import random
 
-colors = [Fore.GREEN,Fore.RED]
+colors = [GREEN,RED,BLUE]
+while True:
 
-random_color = random.choice(colors);
-
-print(random_color+"test")
+  random_color = random.choice(colors);
+  time.sleep(0.1)
+  print(random_color+"test")
