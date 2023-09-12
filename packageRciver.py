c6306dfd021ad051d5775766476df12b572227b8
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-09-12 16:00:15 -0400

    packge reciver

--- packageRciver.py
+++ packageRciver.py
@@ -8,4 +8,4 @@ def packet_callback(packet):
 # Set the 'iface' parameter to specify the Wi-Fi interface to capture on
 # Set the 'count' parameter to specify the number of packets to capture
 # Set the 'prn' parameter to the callback function to process captured packets
-sniff(iface="wlan0", count=10, prn=packet_callback)
+sniff(iface="eth0", count=10, prn=packet_callback)
