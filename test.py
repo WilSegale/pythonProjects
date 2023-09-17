f5d3cad4a69335125d6ddfc43f8dadc959451595
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-09-17 14:52:11 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+import ctypes
+
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+if is_admin():
+    print("The script is running with administrative privileges.")
+else:
+    print("The script is NOT running with administrative privileges.")
+
