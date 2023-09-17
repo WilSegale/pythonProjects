9fc11d7c4c178b69bd95bcc0e1341dce2f474d80
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-09-17 09:14:24 -0400

    pie

--- pie-charts.py
+++ pie-charts.py
@@ -3,7 +3,7 @@ import shutil
 
 total,used,free = shutil.disk_usage("/")
 labels = ['Used', 'Free']
-values = [used,free]
+values = [used, free]
 
 # Use `hole` to create a donut-like pie chart
 fig = go.Figure(data = [go.Pie(labels=labels, values=values, hole=0.5)])
