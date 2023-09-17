870f5229e060efba2a6ad0d6c077c23ece465fde
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-09-17 09:18:30 -0400

    output

--- DiskToCSV.py
+++ DiskToCSV.py
@@ -2,20 +2,23 @@ from DontEdit import *
 import pandas as pd
 import shutil
 
+try:
+    total, used, free = shutil.disk_usage("/")
 
-total, used, free = shutil.disk_usage("/")
+    # Data for the CSV chart
+    data = {
+            'Total': [total // (2 ** 30)],
+            'Free':  [free // (2 ** 30)],
+            'Used':  [used // (2 ** 30)]
+        }
 
-# Data for the CSV chart
-data = {
-        'Total': [total // (2 ** 30)],
-        'Free':  [free // (2 ** 30)],
-        'Used':  [used // (2 ** 30)]
-    }
+    # Create a DataFrame from the data
+    df = pd.DataFrame(data)
+    # Output the DataFrame to a CSV file
+    df.to_csv('DiskSpace.csv', index=False)
+    print(BRIGHT + GREEN +"Total: %d GB " % (total // (2 ** 30)))
+    print(RED + "Used: %d GB " % (used // (2 ** 30)))
+    print(YELLOW + "Free: %d GB " % (free // (2 ** 30)) + RESET)
 
-# Create a DataFrame from the data
-df = pd.DataFrame(data)
-# Output the DataFrame to a CSV file
-df.to_csv('DiskSpace.csv', index=False)
-print(BRIGHT+GREEN +"Total: %d GB " % (total // (2 ** 30)))
-print(RED +"Used: %d GB " % (used // (2 ** 30)))
-print(YELLOW +"Free: %d GB " % (free // (2 ** 30))+ Fore.RESET)
+except KeyboardInterrupt:
+    print("GOOD BYE!")
