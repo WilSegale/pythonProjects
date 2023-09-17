e5d52a9e64680d735286322726fed151715aa2ac
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-09-17 09:12:35 -0400

    encryptAFile

--- encryptAFile.py
+++ encryptAFile.py
@@ -1,16 +1,21 @@
 import random
 import string
-print("Input a file name.")
-file_path = input(">>> ")  # Replace with your file's path
 
-def generate_random_string(length):
-    characters = string.ascii_letters + string.digits
-    random_string = ''.join(random.choice(characters) for _ in range(length))
-    return random_string
+try:
+    print("Input a file name.")
+    file_path = input(">>> ")  # Replace with your file's path
 
-length = int(input("Enter the desired length of the random string: "))
-random_string = generate_random_string(length)
+    def generate_random_string(length):
+        characters = string.ascii_letters + string.digits
+        random_string = ''.join(random.choice(characters) for _ in range(length))
+        return random_string
 
-# Open the file in write mode to rewrite its content
-with open(file_path, "w") as file:
-    file.write(random_string)
+    length = int(input("Enter the desired length of the random string: "))
+    random_string = generate_random_string(length)
+
+    # Open the file in write mode to rewrite its content
+    with open(file_path, "w") as file:
+        file.write(random_string)
+        
+except KeyboardInterrupt:
+    print("\nExiting...")
