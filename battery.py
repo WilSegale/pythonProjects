5e090c7dc9d21f733167a117f4645b37b3f9392a
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-09-17 15:00:15 -0400

    battery

--- battery.py
+++ battery.py
@@ -1,29 +1,66 @@
-# python script showing battery details
 from DontEdit import *
 import psutil
+import ctypes
 
-if os.geteuid() == 0:
-    print(f"{BRIGHT}{RED}WARNING:{RESET} It's recommended not to run this program with sudo.")
-    print(f"Running the program with sudo privileges might have unintended consequences.")
-    print(f"Consider running the program without sudo.")
-    exit(1)
-else:
-    battery = psutil.sensors_battery()
-
-    def convertTime(seconds):
-        minutes, seconds = divmod(seconds, 60)
-        hours, minutes = divmod(minutes, 60)
-        return "%d:%02d:%02d" % (hours, minutes, seconds)
-
-    if battery:
-        battery = psutil.sensors_battery()
-        if battery.power_plugged == 0:
-            print(f"{BRIGHT}{YELLOW}Battery percentage: {GREEN}{battery.percent}%")
-            print(f"{YELLOW}Power plugged in: {RED}{battery.power_plugged}")
-            print(f"{YELLOW}Battery left: {GREEN}{convertTime(battery.secsleft)}{RESET}")
+if platform != "win32":
+    def MacOrLinux():
+        if os.geteuid() == 0:
+            print(f"{BRIGHT}{RED}WARNING:{RESET} It's recommended not to run this program with sudo.")
+            print(f"Running the program with sudo privileges might have unintended consequences.")
+            print(f"Consider running the program without sudo.")
+            exit(1)
         else:
-            print(f"{BRIGHT}{YELLOW}Battery percentage: {GREEN}{battery.percent}%")
-            print(f"{YELLOW}Power plugged in: {GREEN}{battery.power_plugged}")
-            print(f"{YELLOW}Battery left: {RED}{convertTime(battery.secsleft)}{RESET}")
-    else:
-        print(f"{RED}{BRIGHT}[-] No battery found.{RESET}")
+            battery = psutil.sensors_battery()
+
+            def convertTime(seconds):
+                minutes, seconds = divmod(seconds, 60)
+                hours, minutes = divmod(minutes, 60)
+                return "%d:%02d:%02d" % (hours, minutes, seconds)
+
+            if battery:
+                battery = psutil.sensors_battery()
+                if battery.power_plugged == 0:
+                    print(f"{BRIGHT}{YELLOW}Battery percentage: {GREEN}{battery.percent}%")
+                    print(f"{YELLOW}Power plugged in: {RED}{battery.power_plugged}")
+                    print(f"{YELLOW}Battery left: {GREEN}{convertTime(battery.secsleft)}{RESET}")
+                else:
+                    print(f"{BRIGHT}{YELLOW}Battery percentage: {GREEN}{battery.percent}%")
+                    print(f"{YELLOW}Power plugged in: {GREEN}{battery.power_plugged}")
+                    print(f"{YELLOW}Battery left: {RED}{convertTime(battery.secsleft)}{RESET}")
+            else:
+                print(f"{RED}{BRIGHT}[-] No battery found.{RESET}")
+    MacOrLinux()
+else:
+    def Windows():
+        # Check if the script is running with administrative privileges
+        def is_admin():
+            try:
+                return ctypes.windll.shell32.IsUserAnAdmin()
+            except:
+                return False
+
+        if is_admin():
+            print(f"{BRIGHT}{RED}WARNING:{RESET} It's recommended not to run this program with administrative privileges.")
+            print(f"Running the program with admin privileges might have unintended consequences.")
+            print(f"Consider running the program without admin privileges.")
+        else:
+            battery = psutil.sensors_battery()
+
+            def convertTime(seconds):
+                minutes, seconds = divmod(seconds, 60)
+                hours, minutes = divmod(minutes, 60)
+                return "%d:%02d:%02d" % (hours, minutes, seconds)
+
+            if battery:
+                if battery.power_plugged == 0:
+                    print(f"{BRIGHT}{YELLOW}Battery percentage: {GREEN}{battery.percent}%")
+                    print(f"{YELLOW}Power plugged in: {RED}{battery.power_plugged}")
+                    print(f"{YELLOW}Battery left: {GREEN}{convertTime(battery.secsleft)}{RESET}")
+                else:
+                    print(f"{BRIGHT}{YELLOW}Battery percentage: {GREEN}{battery.percent}%")
+                    print(f"{YELLOW}Power plugged in: {GREEN}{battery.power_plugged}")
+                    print(f"{YELLOW}Battery left: {RED}{convertTime(battery.secsleft)}{RESET}")
+            else:
+                print(f"{RED}{BRIGHT}[-] No battery found.{RESET}")
+
+    Windows()
