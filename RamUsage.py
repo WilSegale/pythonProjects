08ec34765a6decedf87cd88590fbcc67cd8c1fd8
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-09-16 21:01:53 -0400

    ram Usage

--- RamUsage.py
+++ RamUsage.py
@@ -36,23 +36,23 @@ else:
         total_ram = virtual_memory.total
         return used_ram, total_ram
 
-def get_ram_usage_percentage():
-    ram_percentage = psutil.virtual_memory().percent
-    return ram_percentage
+    def get_ram_usage_percentage():
+        ram_percentage = psutil.virtual_memory().percent
+        return ram_percentage
 
-if __name__ == "__main__":
-    used_ram, total_ram = get_ram_usage()
-    ram_percentage = get_ram_usage_percentage()
+    if __name__ == "__main__":
+        used_ram, total_ram = get_ram_usage()
+        ram_percentage = get_ram_usage_percentage()
 
-    
-    if ram_percentage >= 50:
-        print(f"{BRIGHT}{RED}Warning: RAM usage is high!{RESET}")
-        print(f"Used RAM: {used_ram / (1024 ** 3):.2f} GB")
-        print(f"Total RAM: {total_ram / (1024 ** 3):.2f} GB")
-        print("Amount of RAM left to use: {:.2f} GB".format((total_ram - used_ram) / (1024 ** 3)))
-        print(f"RAM Usage Percentage: {BRIGHT}{RED}{ram_percentage:.2f}%{RESET}")
-    else:
-        print(f"Used RAM: {used_ram / (1024 ** 3):.2f} GB")
-        print(f"Total RAM: {total_ram / (1024 ** 3):.2f} GB")
-        print("Amount of RAM left to use: {:.2f} GB".format((total_ram - used_ram) / (1024 ** 3)))
-        print(f"RAM Usage Percentage: {ram_percentage:.2f}%")
+        
+        if ram_percentage >= 50:
+            print(f"Warning: RAM usage is high!")
+            print(f"Used RAM: {used_ram / (1024 ** 3):.2f} GB")
+            print(f"Total RAM: {total_ram / (1024 ** 3):.2f} GB")
+            print("Amount of RAM left to use: {:.2f} GB".format((total_ram - used_ram) / (1024 ** 3)))
+            print(f"RAM Usage Percentage: {ram_percentage:.2f}%")
+        else:
+            print(f"Used RAM: {used_ram / (1024 ** 3):.2f} GB")
+            print(f"Total RAM: {total_ram / (1024 ** 3):.2f} GB")
+            print("Amount of RAM left to use: {:.2f} GB".format((total_ram - used_ram) / (1024 ** 3)))
+            print(f"RAM Usage Percentage: {ram_percentage:.2f}%")
