9c51883c3a8a7c146242bb06b31cc264253382f8
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-09-23 19:43:53 -0400

    windows

--- windows whats inside the drive.py
+++ windows whats inside the drive.py
@@ -1,33 +1,38 @@
+from DontEdit import *
 import os
 
-def list_drive_contents(drive_path):
-    try:
-        contents = os.listdir(drive_path)
-        return contents
-    except OSError as e:
-        print(f"Error: {e}")
-        return []
+try:
+    def list_drive_contents(drive_path):
+        try:
+            contents = os.listdir(drive_path)
+            return contents
+        except OSError as e:
+            print(f"Error: {e}")
+            return []
 
-def main():
-    drives = ["C:\\", "D:\\", "E:\\"]  # Add the drive letters you want to check
-    print("Available drives:")
-    for drive in drives:
-        if os.path.exists(drive):
-            print(drive)
-    
-    drive_name = input("Enter the drive letter (e.g., C:): ")
-    drive_path = drive_name + "\\"
-    
-    if not os.path.exists(drive_path):
-        print("Drive not found.")
-        return
-    
-    contents = list_drive_contents(drive_path)
-    print(f"Contents of drive {drive_name}:")
-    print("+"*25)
-    
-    for item in contents:
-        print(f"{item}")
+    def main():
+        drives = ["C:\\", "D:\\", "E:\\"]  # Add the drive letters you want to check
+        print("Available drives:")
+        for drive in drives:
+            if os.path.exists(drive):
+                print(drive)
+        
+        drive_name = input("Enter the drive letter (e.g., C:): ")
+        drive_path = drive_name + "\\"
+        
+        if not os.path.exists(drive_path):
+            print("Drive not found.")
+            return
+        
+        contents = list_drive_contents(drive_path)
+        print(f"Contents of drive {drive_name}:")
+        print("+"*25)
+        
+        for item in contents:
+            print(f"{item}")
 
-if __name__ == "__main__":
-    main()
+    if __name__ == "__main__":
+        main()
+
+except KeyboardInterrupt:
+    print("\n[-]EXITING...")
