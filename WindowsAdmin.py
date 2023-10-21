ebf35e20925f4e28cdef202f2e2609f6c078ae8f
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-10-21 16:33:22 -0400

    admin

--- WindowsAdmin.py
+++ WindowsAdmin.py
@@ -1,2 +1,12 @@
 import ctypes
 
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+if is_admin():
+    print("You are running with administrative privileges.")
+else:
+    print("You are not running with administrative privileges.")
