3187600ac37c3e1e45cb8cc9e56cfae7e717dab4
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-10-21 11:34:08 -0400

    random color

--- RandomColor.py
+++ RandomColor.py
@@ -2,6 +2,7 @@ from DontEdit import *
 import random
 
 colors = [GREEN,RED,BLUE]
+
 while True:
   random_color = random.choice(colors);
-  print(random_color+"test")
+  print(f"{random_color}test")
