d18bbb965697041b95d94aa49bb32b9f5544b12b
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-10-21 11:42:35 -0400

    fileinfo

--- FileInfo.py
+++ FileInfo.py
@@ -4,8 +4,8 @@ file_path = os.path.expanduser("~/Desktop/")
 
 file_info = os.stat(file_path)
 
-print('Size:', file_info.st_size)
-print('Creation Time:', file_info.st_ctime)
-print('Permissions:', oct(file_info.st_mode))
-print('Name:', os.path.basename(file_path))
-print('Is Directory:', os.path.isdir(file_path))
+print(f'Size: {file_info.st_size}')
+print(f'Creation Time: {file_info.st_ctime}')
+print(f'Permissions: {oct(file_info.st_mode)}')
+print(f'Name: {os.path.basename(file_path)}')
+print(f'Is Directory: {os.path.isdir(file_path)}')
