28d62c78a2c3a81f3cb9a5a54f5fd7d15c3b4da5
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-10-24 12:45:21 -0400

    table

--- periodic_table.py
+++ periodic_table.py
@@ -123,11 +123,27 @@ elements = [
 ]
 
 # Remove the unused index 0
-
-file_name = "elements.json"
+elements.pop(0)
 
 # Write the data to the JSON file
+file_name = "elements.json"
 with open(file_name, "w") as json_file:
-    json.dump(elements, json_file, indent=0)
+    json.dump(elements, json_file, indent=4)
 
 print(f"Data has been written to {file_name}.")
+
+# Input a number from 1-118 to select an element
+ChosenElement = input("Input an atomic number (1-118) to select an element: ")
+
+try:
+    ChosenElement = int(ChosenElement)
+    if 1 <= ChosenElement <= 118:
+        element = next((el for el in elements if el["atomic_number"] == ChosenElement), None)
+        if element:
+            print(element)
+        else:
+            print("Element not found.")
+    else:
+        print("Invalid atomic number.")
+except ValueError:
+    print("Invalid input. Please enter a valid atomic number.")
