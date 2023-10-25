5add1919479c713e82af49513f9265825fb6c78c
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-10-25 12:23:17 -0400

    die

--- diagnostics.py
+++ diagnostics.py
@@ -1,6 +1,6 @@
 import subprocess
 
-diagnostic = open("diagnostics.txt", "a")
+diagnostic = open("diagnostics.txt", "w")
 
 try:
     # Define a function to run shell commands and capture their output
@@ -16,7 +16,19 @@ try:
         "ping google.com -t 3",
         "traceroute google.com",
         "csrutil status",
-        "sudo pfctl -s info"
+        "sudo pfctl -s info",
+        "system_profiler SPSoftwareDataType",
+        "networksetup -listallnetworkservices",
+        "networksetup -listallhardwareports",
+        "df -h",
+        "mount",
+        "ps aux",
+        "ps -ef | grep -v grep | awk '{print $2}'",
+        "dscl . -list /Users",
+        "cat /var/log/system.log",
+        "nslookup google.com",
+        "sudo pfctl -sa",
+        "vm_stat"
     ]
 
     # Execute each diagnostic command and print the output
