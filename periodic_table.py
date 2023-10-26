146b2d31a6c5e169c659641d0bfd13380d96ec6c
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-10-26 12:16:26 -0400

    python

--- periodic_table.py
+++ periodic_table.py
@@ -1,10 +1,11 @@
-from DontEdit import json
+from DontEdit import *
 from elements import *
 from htmlfile import *
 
 file_name = "elements.json"
 with open(file_name, "w") as json_file:
     json.dump(elements, json_file, indent=4)
+print(f"Data has been written to {file_name}.")
 
 # Corrected function name
 def AtomicNumber():
@@ -31,7 +32,7 @@ def AtomicNumber():
                 else:
                     print("Element not found.")
             else:
-                print("Invalid atomic number.")
+                print(f"{BRIGHT}{RED}[*]{RESET} Invalid Atomic Number.")
     except KeyboardInterrupt:
         print("\n[-] Keyboard Interrupt")
 
@@ -58,22 +59,54 @@ def AtomicName():
                 with open(f"element_{chosen_element['atomic_number']}.html", "w") as html_file:
                     html_file.write(html_output)
                 print(f"HTML file 'element_{chosen_element['atomic_number']}.html' has been generated.")
-    
+            else:
+                print(f'{BRIGHT}{RED}[*]{RESET} Atomic Name not found ')
     except KeyboardInterrupt:
         print("\n[-] Keyboard Interrupt")
 
+
+def symbol():
+    try:
+        while True:
+            ChosenElement = input("Input an symbol to select an element (or type 'exit' to quit): ")
+            if ChosenElement.lower() == 'exit':
+                break
+
+            chosen_element = None
+            for element in elements:
+                if element["symbol"].lower() == ChosenElement.lower():
+                    chosen_element = element
+                    break
+
+            if chosen_element:
+                print(f'\nName: {chosen_element["name"]} [{element["symbol"]}]', end=" ")
+                print(f'Atomic Number: {chosen_element["atomic_number"]}', end=" ")
+                print(f'Atomic Weight: {chosen_element["atomic_weight"]}\n')
+
+                # Generate an HTML file for the selected element
+                html_output = generate_element_html(chosen_element)  # Make sure you have the 'generate_element_html' function implemented.
+                with open(f"element_{chosen_element['atomic_number']}.html", "w") as html_file:
+                    html_file.write(html_output)
+                print(f"HTML file 'element_{chosen_element['atomic_number']}.html' has been generated.")
+            else:
+                print(f'{BRIGHT}{RED}[*]{RESET} Symbol not found')
+    except KeyboardInterrupt:
+        print(f"\n{BRIGHT}{RED}[*]{RESET} Keyboard Interrupt")
+
 # Write the data to the JSON file
 
-print(f"Data has been written to {file_name}.")
 
 # Input a number from 1-118 to select an element
-choices = int(input("Do you want to see the atomic number(1). Or element name(2): "))
+choices = int(input("Do you want to see the atomic number(1), or element name(2). Or symbol (3): "))
 
 if choices == 1:
     AtomicNumber()
 
 elif choices == 2:
     AtomicName()
-    
+
+elif choices == 3:
+    symbol()
+
 else:
-    print("Invalid choice. Please enter 1 or 2.")
+    print("Invalid choice. Please enter 1, 2 or 3.")
