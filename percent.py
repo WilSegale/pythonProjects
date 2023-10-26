db4bc285ba151df0d0dad296efe364690a860bed
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-10-26 12:24:13 -0400

    percent

--- percent.py
+++ percent.py
@@ -6,7 +6,6 @@ def percent():
     whole = int(float(input('''Enter the whole number ''')))
     inputFile = input("would you like to put the number into a file so you can read it later? ")
     if inputFile in no:
-        
         print(f"{part/whole*100 :,.2f}% of the number")
     elif inputFile in yes:
         print(f"{part/whole*100 :,.2f}% of the number")
