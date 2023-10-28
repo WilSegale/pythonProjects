e7c9857c7b24be11668d99c059ddad51f4ff580f
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-10-28 09:56:58 -0400

    KG

---
+++ KG to LBS.py
@@ -0,0 +1,9 @@
+# Function to convert kilograms to pounds
+def kg_to_lbs(kg):
+    lbs = kg * 2.20462
+    return lbs
+
+# Example usage
+kg = int(input("Input the KG you want to covert to LBS: "))
+lbs = kg_to_lbs(kg)
+print(f"{kg:,.2f} kilograms is equal to {lbs:,.2f} pounds")
