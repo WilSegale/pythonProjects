06fed5deb3e7fefc5cb06885671cee2600972ff2
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-11-03 10:58:58 -0400

    Update old year to now.py

--- old year to now.py
+++ old year to now.py
@@ -1,3 +1,4 @@
+from DontEdit import *
 import datetime
 
 
@@ -8,7 +9,7 @@ try:
         old_year = int(input("Input the year you want to know how long ago was: "))
         current_year = datetime.datetime.now().year
         if old_year == current_year:
-            print("The year you put in is today. Plase input a diffrent year")
+            print(f"{BRIGHT}{RED}[-]{RESET} The year you put in is today. Plase input a diffrent year")
             return year()
         else:
             print(f"The year '{old_year}' was '{current_year - old_year}' years ago.")
