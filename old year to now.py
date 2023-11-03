2bd972c865407d091e21264b7f2dd5c93e805823
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-11-03 10:52:06 -0400

    html

--- old year to now.py
+++ old year to now.py
@@ -1,8 +1,10 @@
 import datetime
 
 try:
-    old_year = int(input("Input the year you want to know how long ago was: "))
-    current_year = datetime.datetime.now().year
-    print(f"The year '{old_year}' was '{current_year - old_year}' years ago.")
+    def year():
+        old_year = int(input("Input the year you want to know how long ago was: "))
+        current_year = datetime.datetime.now().year
+        print(f"The year '{old_year}' was '{current_year - old_year}' years ago.")
+    year()
 except KeyboardInterrupt:
     print('error: KeyboardInterrupt')
