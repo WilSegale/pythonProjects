f8dbe5e126ff379f6e4a15f22829589a2cf6ee4c
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-11-03 10:57:38 -0400

    old yer to  today

--- old year to now.py
+++ old year to now.py
@@ -1,10 +1,18 @@
 import datetime
 
+
+dateTime = open("YearToYear.txt", "a")
+
 try:
     def year():
         old_year = int(input("Input the year you want to know how long ago was: "))
         current_year = datetime.datetime.now().year
-        print(f"The year '{old_year}' was '{current_year - old_year}' years ago.")
+        if old_year == current_year:
+            print("The year you put in is today. Plase input a diffrent year")
+            return year()
+        else:
+            print(f"The year '{old_year}' was '{current_year - old_year}' years ago.")
+            print(f"The year '{old_year}' was '{current_year - old_year}' years ago.",file=dateTime)
     year()
 except KeyboardInterrupt:
-    print(f'\nerror: KeyboardInterrupt')
+    print(f'\nKeyboardInterrupt')
