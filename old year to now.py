cac1f8a9012a6991f4d74d08a86944a7a73d9a55
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-11-12 11:04:01 -0500

    old year to today

--- old year to now.py
+++ old year to now.py
@@ -11,6 +11,8 @@ try:
         if old_year == current_year:
             print(f"{BRIGHT}{RED}[-]{RESET} The year you put in is today. Plase input a diffrent year")
             return year()
+        elif old_year > current_year:
+            print("That year doesn't exist yet. Plase input a diffrent year")
         else:
             print(f"The year '{old_year}' was '{current_year - old_year}' years ago.")
             print(f"The year '{old_year}' was '{current_year - old_year}' years ago.",file=dateTime)
