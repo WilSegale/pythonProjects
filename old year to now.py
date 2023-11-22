ed2f46d7c49c67b6e898f94121f35aceb47bb986
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-11-22 08:16:07 -0500

    year to year

--- old year to now.py
+++ old year to now.py
@@ -8,14 +8,20 @@ try:
     def year():
         old_year = int(input("Input the year you want to know how long ago was: "))
         current_year = datetime.datetime.now().year
+        YearsAgo = current_year - old_year
         if old_year == current_year:
             print(f"{BRIGHT}{RED}[-]{RESET} The year you put in is today. Plase input a diffrent year")
             return year()
         elif old_year > current_year:
             print("That year doesn't exist yet. Plase input a diffrent year")
         else:
-            print(f"The year '{old_year}' was '{current_year - old_year}' years ago.")
-            print(f"The year '{old_year}' was '{current_year - old_year}' years ago.",file=dateTime)
+            if YearsAgo == int(1):
+                print(f"\nThe year '{old_year}' was '{YearsAgo}' year ago.")
+            elif YearsAgo > int(1):
+                print(f"\nThe year '{old_year}' was '{YearsAgo}' years ago.")
+
+            else:
+                print(f"\nThe year '{old_year}' was '{current_year - old_year}' years ago.",file=dateTime)
     year()
 except KeyboardInterrupt:
     print(f'\nKeyboardInterrupt')
