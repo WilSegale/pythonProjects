2e7c7f92feeb9b98425fcba8c6a140872f2387ce
Will Segale <39059459+WilSegale@users.noreply.github.com>
2023-11-25 10:35:57 -0500

    old yer

--- old year to now.py
+++ old year to now.py
@@ -1,27 +1,32 @@
 from DontEdit import *
 import datetime
 
-
-dateTime = open("YearToYear.txt", "a")
-
-try:
+with open("YearToYear.txt", "w") as dateTime:
     def year():
-        old_year = int(input("Input the year you want to know how long ago was: "))
+        try:
+            old_year = int(input("Input the year you want to know how long ago was: "))
+        except ValueError:
+            print("Invalid input. Please enter a valid year.")
+            return year()
+
         current_year = datetime.datetime.now().year
         YearsAgo = current_year - old_year
-        if old_year == current_year:
-            print(f"{BRIGHT}{RED}[-]{RESET} The year you put in is today. Plase input a diffrent year")
-            return year()
-        elif old_year > current_year:
-            print("That year doesn't exist yet. Plase input a diffrent year")
-        else:
-            if YearsAgo == int(1):
-                print(f"\nThe year '{old_year}' was '{YearsAgo}' year ago.")
-            elif YearsAgo > int(1):
-                print(f"\nThe year '{old_year}' was '{YearsAgo}' years ago.")
 
+        if old_year == current_year:
+            print(f"{BRIGHT}{RED}[-]{RESET} The year you put in is today. Please input a different year")
+        elif old_year > current_year:
+            print("That year doesn't exist yet. Please input a different year")
+        else:
+            if YearsAgo == 1:
+                print(f"\nThe year '{old_year}' was '{YearsAgo}' year ago.")
+            elif YearsAgo > 1:
+                print(f"\nThe year '{old_year}' was '{YearsAgo}' years ago.")
             else:
-                print(f"\nThe year '{old_year}' was '{current_year - old_year}' years ago.",file=dateTime)
-    year()
-except KeyboardInterrupt:
-    print(f'\nKeyboardInterrupt')
+                print(f"\nThe year '{old_year}' was '{YearsAgo}' years ago.", file=dateTime)
+
+    while True:
+        try:
+            year()
+            break  # Exit the loop if the year function is executed without exceptions
+        except KeyboardInterrupt:
+            print(f'\nKeyboardInterrupt')
