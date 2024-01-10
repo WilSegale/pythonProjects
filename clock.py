8ae737e77d12553b7f58715801cf358be944f7d1
Will Segale <39059459+WilSegale@users.noreply.github.com>
2024-01-10 09:45:57 -0500

    clock update

--- clock.py
+++ clock.py
@@ -3,16 +3,18 @@ from time import strftime
 from sys import platform
 import time
 import os
-
-if platform == "darwin" or platform == "linux":
-    while True:
-        os.system("clear")
-        tick = strftime('%I:%M:%S %p')
-        print(f"Time: {tick}")
-        time.sleep(1)
-else:
-    while True:
-        os.system('cls')
-        tick = strftime('%I:%M:%S %p')
-        print(f"Time: {tick}")
-        time.sleep(1)
+try:
+    if platform == "darwin" or platform == "linux":
+        while True:
+            os.system("clear")
+            tick = strftime('%I:%M:%S %p')
+            print(f"Time: {tick}")
+            time.sleep(1)
+    else:
+        while True:
+            os.system('cls')
+            tick = strftime('%I:%M:%S %p')
+            print(f"Time: {tick}")
+            time.sleep(1)
+except KeyboardInterrupt:
+    print("\n[-] Quiting clock")
