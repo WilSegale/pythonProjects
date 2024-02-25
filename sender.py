dc1b8b7bd1a7fd8f5bd43f99f40461b53c0338b2
Will Segale <39059459+WilSegale@users.noreply.github.com>
2024-02-25 14:12:36 -0500

    ptrhon

--- sender.py
+++ sender.py
@@ -1,18 +1,9 @@
 import socket
 
-# Create a socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s = socket.socket()
+host = s.gethostname()
+port = 8080
+s.bind((host,port))
 
-# Set up the connection details
-host = '192.168.1.62'  # IP address of the receiving computer
-port = 12345  # Choose a suitable port number
-
-# Connect to the receiver
-s.connect((host, port))
-
-# Send data
-data = b'Hello, receiver!'
-s.send(data)
-
-# Close the socket
-s.close()
+s.listen(1)
+print("waiting for connection")
