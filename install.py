9076ee219da4a4ed87272279baa355b3765f064b
Will Segale <39059459+WilSegale@users.noreply.github.com>
2024-02-25 11:03:44 -0500

    install.py

--- install.py
+++ install.py
@@ -1,8 +1,14 @@
 import subprocess
-from DontEdit import *
+import matplotlib
+matplotlib.use('Agg')
 
 install = input("Would you like to install the requirements for this folder (y/n): ")
 
 if install.lower() == 'y':
     subprocess.run(["pip3", "install", "-r", "requirements.txt"])
     print("Requirements installed successfully")
+elif install.lower() == 'n':
+    print("Requirements installation skipped")
+else:
+    print("Invalid input, requirements installation skipped")
+    
