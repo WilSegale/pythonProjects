0771c0980fc18944d27b8bff0864430baea60a2d
Will Segale <39059459+WilSegale@users.noreply.github.com>
2024-02-28 08:45:00 -0500

    ghz.py

---
+++ GHZ.py
@@ -0,0 +1,10 @@
+import psutil
+
+def get_cpu_frequency():
+    freq = psutil.cpu_freq()
+    if freq is not None:
+        return freq.current / 1000  # Convert to GHz
+    else:
+        return "N/A"
+
+print("CPU Frequency: {} GHz".format(get_cpu_frequency()))
