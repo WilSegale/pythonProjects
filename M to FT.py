784859ab5738d8657eed9aae905059a065107028
WilSegale <39059459+WilSegale@users.noreply.github.com>
2024-06-20 08:01:00 -0400

    Update M to FT.py

--- M to FT.py
+++ M to FT.py
@@ -2,4 +2,4 @@ print("Input the meters in and I will covert it to feet")
 meters = float(input(">>> "))
 feet = meters * 3.28084
 round_feet = round(feet)
-print(f"{round_feet :,.2f}")
+print(f"{round_feet :,.2f} FT")
