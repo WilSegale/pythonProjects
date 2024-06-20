f5d135eafcbbf3fcaaf750521bf42495c7355baf
WilSegale <39059459+WilSegale@users.noreply.github.com>
2024-06-20 01:56:38 -0400

    update

--- encryptAFile.py
+++ encryptAFile.py
@@ -20,23 +20,23 @@ while True:
     input_path = session.prompt('Enter file name: ')
     if input_path == 'exit':
         break
-    print(f'You entered: {input_path}')
 
-try:
-    print("Input a file name.")
-    file_path = input(">>> ")  # Replace with your file's path
+    try:
+        print("Input a file name.")
+        file_path = input(">>> ")  # Replace with your file's path
 
-    def generate_random_string(length):
-        characters = string.ascii_letters + string.digits
-        random_string = ''.join(random.choice(characters) for _ in range(length))
-        return random_string
+        def generate_random_string(length):
+            characters = string.ascii_letters + string.digits
+            random_string = ''.join(random.choice(characters) for _ in range(length))
+            return random_string
 
-    length = int(input("Enter the desired length of the random string: "))
-    random_string = generate_random_string(length)
+        length = int(input("Enter the desired length of the random string: "))
+        random_string = generate_random_string(length)
 
-    # Open the file in write mode to rewrite its content
-    with open(file_path, "w") as file:
-        file.write(random_string)
-        
-except KeyboardInterrupt:
-    print("\n[-] Exiting...")
+        # Open the file in write mode to rewrite its content
+        with open(file_path, "w") as file:
+            file.write(random_string)
+            
+    except KeyboardInterrupt:
+        print("\n[-] Exiting...")
+        break
