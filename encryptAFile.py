92ee984fe4ac992d808e84395b9a0fbd9c2f932a
WilSegale <39059459+WilSegale@users.noreply.github.com>
2024-06-20 02:00:24 -0400

    update

--- encryptAFile.py
+++ encryptAFile.py
@@ -2,7 +2,7 @@ import random
 import string
 import os
 from prompt_toolkit import PromptSession
-from prompt_toolkit.completion import Completer, Completion # type: ignore
+from prompt_toolkit.completion import Completer, Completion
 
 class PathCompleter(Completer):
     def get_completions(self, document, complete_event):
@@ -12,29 +12,22 @@ class PathCompleter(Completer):
             if f.startswith(text):
                 yield Completion(f, start_position=-len(text))
 
-# Create a PromptSession with the custom completer
-session = PromptSession(completer=PathCompleter())
+try:
+    # Create a PromptSession with the custom completer
+    session = PromptSession(completer=PathCompleter())
 
-# Simple REPL loop to demonstrate auto-completion
-while True:
-    input_path = session.prompt('Enter file name: ')
-    if input_path == 'exit':
-        break
-
-    try:
-        
-        def generate_random_string(length):
-            characters = string.ascii_letters + string.digits
-            random_string = ''.join(random.choice(characters) for _ in range(length))
-            return random_string
+    # Simple REPL loop to demonstrate auto-completion
+    while True:
+        input_path = session.prompt('Enter file name: ')
+        if input_path == 'exit':
+            break
 
         length = int(input("Enter the desired length of the random string: "))
-        random_string = generate_random_string(length)
+        random_string = ''.join(random.choices(string.ascii_letters + string.digits, k=length))
 
         # Open the file in write mode to rewrite its content
-        with open(file_path, "w") as file:
+        with open(input_path, "w") as file:
             file.write(random_string)
             
-    except KeyboardInterrupt:
-        print("\n[-] Exiting...")
-        break
+except KeyboardInterrupt:
+    print("\n[-] Exiting...")
