41e6e202f58ba921105e84e777125c8350e83b40
WilSegale <39059459+WilSegale@users.noreply.github.com>
2024-07-11 13:14:25 -0400

    update

--- DataSize.py
+++ DataSize.py
@@ -1,3 +1,5 @@
+from DontEdit import *
+
 def convert_kb_to_other_units(kb):
     mb = kb / 1024
     gb = kb / (1024 * 1024)
@@ -11,7 +13,7 @@ kb_value = float(input("Enter the number of kilobytes (KB): "))
 mb_value, gb_value, tb_value = convert_kb_to_other_units(kb_value)
 
 # Print the results
-print(f"{kb_value} KB is:")
-print(f"{mb_value:.4f} MB")
-print(f"{gb_value:.4f} GB")
-print(f"{tb_value:.10f} TB")
+print(f"{kb_value:,.2f} {GREEN}KB{RESET} is:")
+print(f"{mb_value:,.2f} {GREEN}MB{RESET}")
+print(f"{gb_value:,.2f} {GREEN}GB{RESET}")
+print(f"{tb_value:,.2f} {GREEN}TB{RESET}")
